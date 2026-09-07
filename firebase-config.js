c304baa09a4b8ce31515686dd2cb11f13901ab9d
danielraimann <danielraimann@gmail.com>
2026-09-07 11:32:33 -0300

    Adiciona configuração do Firebase ao simulador

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Esta configuração identifica o aplicativo Web "Simulador Triplex" no Firebase.
+// Ela é pública por definição; o acesso aos dados é protegido pelas regras do Firestore.
+window.FIREBASE_CONFIG = {
+  apiKey: "AIzaSyAckNUJl_YJ69Uy_c2RWQY20YmcicQ8Ii0",
+  authDomain: "caixa-leilao.firebaseapp.com",
+  projectId: "caixa-leilao",
+  storageBucket: "caixa-leilao.firebasestorage.app",
+  messagingSenderId: "694225592516",
+  appId: "1:694225592516:web:047d895bcaab1d6a95e991",
+  measurementId: "G-KEDWENWEF2"
+};
+
